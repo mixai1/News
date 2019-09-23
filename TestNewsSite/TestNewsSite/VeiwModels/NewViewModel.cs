80f7b1265aa143634739dd61284fa8c3fcfdb3ac
Unknown <kachan_mixail@bk.ru>
2019-09-23 18:03:02 +0300

    +ViewAddNew+Metod

---
+++ TestNewsSite/TestNewsSite/VeiwModels/NewViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestNewsSite.VeiwModels
+{
+    public class NewViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string RedirectUrl { get; set; }
+
+        
+        [Display(Name = "News Headilne")]
+        [Required(ErrorMessage = "Heading is required")]
+        public string Heading { get; set; }
+
+    }
+}
